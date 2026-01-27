59e6dc84e2fa259520d937c4b7aee5b33f47bc5c
Nikolay <2097nikola2097@gmail.com>
2026-01-27 11:31:16 +0500

    lab1.1

--- src/practice1/problem4.java
+++ src/practice1/problem4.java
@@ -1,20 +1,39 @@
 package practice1;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class problem4 {
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
 
         double a, b, c, D;
 
         System.out.print("Enter a: ");
+        if (!in.hasNextDouble()) {
+            System.out.println("Invalid input for a");
+            return;
+        }
         a = in.nextDouble();
 
+        if (a == 0) {
+            System.out.println("This is not a quadratic equation.");
+            return;
+        }
+
         System.out.print("Enter b: ");
+        if (!in.hasNextDouble()) {
+            System.out.println("Invalid input for b");
+            return;
+        }
         b = in.nextDouble();
 
         System.out.print("Enter c: ");
+        if (!in.hasNextDouble()) {
+            System.out.println("Invalid input for c");
+            return;
+        }
         c = in.nextDouble();
 
         D = b * b - 4 * a * c;
@@ -22,18 +41,21 @@ public class problem4 {
         if (D < 0) {
             System.out.println("Discriminant is negative, no roots.");
         }
-
-        else if (D == 0) {
+        else if (Math.abs(D) < 1e-9) {
             double x = -b / (2 * a);
-            System.out.println("There is one x = " + x);
+            System.out.println("There is one root: x = " + x);
         }
         else {
-            double sqrt_D = Math.sqrt(D);
+            double sqrtD = Math.sqrt(D);
 
-            double x1 = (-b + sqrt_D) / (2 * a);
-            double x2 = (-b - sqrt_D) / (2 * a);
+            double x1 = (-b + sqrtD) / (2 * a);
+            double x2 = (-b - sqrtD) / (2 * a);
 
-            System.out.println("There are x1 = " + x1 + " and x2 = " + x2);
+            System.out.println("There are two roots:");
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
         }
+
+        in.close();
     }
 }
